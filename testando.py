7546c5f43307d39e08f252ef124204d26c14bdaa
dayson <dayson.portela@gmail.com>
2023-02-22 11:54:51 -0300

    salvando o que guardei depois

--- testando.py
+++ testando.py
@@ -1,5 +1,9 @@
 print('testando')
 print('testando de novo')
 print('testando de terceira ve')
+<<<<<<< Updated upstream
 print('testando de 4ve')
 print('criando alteração')
+=======
+print('testando de 4ve testando para commit depois')
+>>>>>>> Stashed changes
