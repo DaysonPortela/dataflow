bd02be5a95a1024b2add93d2d8522bef6f01e796
dayson <dayson.portela@gmail.com>
2023-02-22 13:02:12 -0300

    ultima alteração

--- testando.py
+++ testando.py
@@ -1,9 +1,5 @@
 print('testando')
 print('testando de novo')
 print('testando de terceira ve')
-<<<<<<< Updated upstream
 print('testando de 4ve')
 print('criando alteração')
-=======
-print('testando de 4ve testando para commit depois')
->>>>>>> Stashed changes
